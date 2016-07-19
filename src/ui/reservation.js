7f262910454ad2e55e0f10f9ba7d44747106b517
Alex Welsh <alex.n.welsh@gmail.com>
2016-07-18 17:23:04 -0700

    Reservation fields, not working

--- src/ui/reservation.js
+++ src/ui/reservation.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 export default React.createClass({
+	
+	handleSubmit: function(e){
+		e.preventDefault()
+
+	},
   render: function () {
     return (
       <div>
       	<h1> Reservation </h1>
+      		<form>
+      			Full Name <br />
+		      	<input type="text" className="dog"/><br />
+		      	Number of Guests <br />
+		      	<input type="number" min="1" max="12"/><br />
+		      	Date <br />
+		      	<input type="date" /><br />
+				<select>
+					<option> Indoor</option>
+					<option> Outdoor</option>
+				</select> <br />
+				<button onClick={this.handleSubmit} type="submit">Reserve Table</button>
 
+      		</form>
       </div>
     )
   }
